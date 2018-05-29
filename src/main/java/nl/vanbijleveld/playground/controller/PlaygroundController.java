88b39ecd39e41f377c2aec656cbd159a456cf3d3
Jeffrey <jeffrey@vanbijleveld.nl>
2018-05-29 11:01:44 +0200

    debugging SMTP

--- src/main/java/nl/vanbijleveld/playground/controller/PlaygroundController.java
+++ src/main/java/nl/vanbijleveld/playground/controller/PlaygroundController.java
@@ -1,6 +1,7 @@
 package nl.vanbijleveld.playground.controller;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 import javax.mail.MessagingException;
@@ -9,12 +10,15 @@ import nl.vanbijleveld.imapreader.ImapService;
 import nl.vanbijleveld.imapreader.entities.Attachment;
 import nl.vanbijleveld.imapreader.entities.Email;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlaygroundController {
+    private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
     @Value("${email.smtp.host}")
     private String emailHost;
@@ -33,23 +37,30 @@ public class PlaygroundController {
     @RequestMapping(value = "/imap")
     public String emailTester() {
         String ret = "The mail I found..<br><br>";
+
+        LOGGER.debug("Start fetching new mail");
         try {
             ImapService mailService = new ImapService(emailHost, emailUser, emailPass);
+            LOGGER.debug("Connected to host {} with user {}", emailHost, emailUser);
             List<Email> newMessages = mailService.getAllNewEmail();
             for (Email mail : newMessages) {
+
+                LOGGER.debug("Found new email from {} received on {}", mail.getPrettyFrom(), mail.getReceivedDate());
                 ret += mail.getSubject() + " received on " + mail.getReceivedDate() + " from " + mail.getPrettyFrom() + "<br>";
 
-                for (Attachment attachment : mail.getAttachments()) {
-                    ret += "............" + attachment.getFileName() + "<br>";
-
+                if (mail.getAttachments() != null) {
+                    for (Attachment attachment : mail.getAttachments()) {
+                        ret += "............" + attachment.getFileName() + "<br>";
+                        LOGGER.debug("Found attachment {}", attachment.getFileName());
+                    }
                 }
                 ret += "<br>";
-                System.out.println("processing mail: " + mail.getSubject() + " received on " + mail.getReceivedDate() + " from " + mail.getPrettyFrom());
 
             }
 
             return ret;
         } catch (MessagingException | IOException e) {
+            LOGGER.error("Failed to process email", e);
             return "Error connecting mailbox: " + e.getMessage();
         }
     }
