c71a465ac08f0cf886ffb830a1b082ba12fee487
Jeffrey <jeffrey@vanbijleveld.nl>
2018-05-29 09:09:05 +0200

    debugging SMTP

--- src/main/java/nl/vanbijleveld/playground/controller/PlaygroundController.java
+++ src/main/java/nl/vanbijleveld/playground/controller/PlaygroundController.java
@@ -1,9 +1,12 @@
 package nl.vanbijleveld.playground.controller;
 
-import javax.mail.Message;
+import java.io.IOException;
+import java.util.List;
+
 import javax.mail.MessagingException;
 
 import nl.vanbijleveld.imapreader.ImapService;
+import nl.vanbijleveld.imapreader.entities.Email;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,15 +34,15 @@ public class PlaygroundController {
         String ret = "The mail I found..<br><br>";
         try {
             ImapService mailService = new ImapService(emailHost, emailUser, emailPass);
-            Message[] newMessages = mailService.checkForNewEmail();
-            for (Message mail : newMessages) {
+            List<Email> newMessages = mailService.getAllNewEmail();
+            for (Email mail : newMessages) {
                 ret += mail.getSubject() + " received on " + mail.getReceivedDate() + " from " + mail.getFrom() + "<br>";
                 System.out.println("processing mail: " + mail.getSubject() + " received on " + mail.getReceivedDate() + " from " + mail.getFrom().toString());
 
             }
 
             return ret;
-        } catch (MessagingException e) {
+        } catch (MessagingException | IOException e) {
             return "Error connecting mailbox: " + e.getMessage();
         }
     }
